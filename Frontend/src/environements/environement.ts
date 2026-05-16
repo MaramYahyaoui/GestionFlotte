92b8aca271d0c9517d7d26e18f362201045a3a16
MaramYahyaoui <yahyaouimaram20@gmail.com>
2026-05-16 21:21:36 +0100

    Fix production API URL

--- Frontend/src/environements/environement.ts
+++ Frontend/src/environements/environement.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9090/api'  // ← votre backend tourne sur 9090 donc OK
+  apiUrl: 'http://34.52.144.217:9090/api'  // ← votre backend tourne sur 9090 donc OK
 };
